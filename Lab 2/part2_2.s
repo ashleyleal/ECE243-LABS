2cfbb4b03608f6f6be7db84e92295dbe603a6d49
Ashley N Leal <69736735+ashleyleal@users.noreply.github.com>
2024-01-20 15:55:46 -0500

    fixed alignment issue using .align 2

--- Lab 2/part2_2.s
+++ Lab 2/part2_2.s
@@ -24,7 +24,7 @@ loop:
     beq r14, r0, finished  # if end of list send the default value
     addi r11, r11, 4    # add 4 to pointer to the numbers to point to next one
     addi r12, r12, 1    # increment index
-    ldw  r14, 0(r11)    # load next student number
+    ldw  r14, (r11)    # load next student number
     br loop
 	
 calculate:
@@ -41,8 +41,8 @@ iloop: br iloop
 /* result should hold the grade of the student number put into r10, or
 -1 if the student number isn't found */ 
 
-
 result: .byte 0
+.align 2 #fix address alighment issue (fill 0s)
 		
 /* Snumbers is the "array," terminated by a zero of the student numbers  */
 Snumbers: .word 10392584, 423195, 644370, 496059, 296800
@@ -50,9 +50,12 @@ Snumbers: .word 10392584, 423195, 644370, 496059, 296800
         .word 982966, 345018, 220809, 369328, 935042
         .word 467872, 887795, 681936, 0
 
+
 /* Grades is the corresponding "array" with the grades, in the same order*/
 Grades: .byte 99, 68, 90, 85, 91, 67, 80
         .byte 66, 95, 91, 91, 99, 76, 68  
         .byte 69, 93, 90, 72
 	
+	
+
 	
