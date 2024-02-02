386bd0ef3fad92a1dccf33e29b8ec1eeb357b6b3
Ashley N Leal <69736735+ashleyleal@users.noreply.github.com>
2024-02-02 17:58:08 -0500

    Can control leds individually but need to implement lab

--- Lab 4/part1.s
+++ Lab 4/part1.s
@@ -1,58 +1,46 @@
 .global _start
 _start:
-
-	.equ LEDs, 0xFF200000
-	.equ BUTTONs, 0xFF200050
-	
-	movi r6, 0 # Store value of the output
-	movi r5, 15 # Max output
-	
-	movi r10, 1
-	movi r11, 2
-	movi r12, 4
-	movi r13, 8
-	
-ButtonToggleLED:
-	movia r8, LEDs
-	movia r9, BUTTONs
-	stwio r0, 0(r8) #turn off all LEDs, write 0 to LED PIT DR
-
-FEVER: #polling, constantly check for press
-
-      ldwio r2, 12(r9) #read BUTTON PIT EDGE
-      beq r2, r0, FEVER # not pressed yet, keep checking
-
-pressed:   
-     # ldwio r4, 0(r8) # read LED DR (current status)
-     # xori r4, r4, 1 # toggle led 0
-	  
-	  beq r2, r10, KEY0
-	  beq r2, r11, KEY1
-	  beq r2, r12, KEY2
-	  bge r2, r13, KEY3
-	  
-toggle:
-      stwio r6, 0(r8) # change the LEDs (write to LED PIT DR)
-      movi r2, 1 #      RESET bit 0 of BUTTON PIT EDGE
-      stwio r2, 12(r9) # write the DR of the LED PIT
-      br FEVER
-	
-
-KEY0:
-	movi r6, 1
-	br toggle
-
-KEY1:
-	bge r6, r5, FEVER #change nothing, keep polling because at upper limit
-	addi r6, r6, 1
-	br toggle
-	
-
-KEY2:
-	ble r6, r0, FEVER #change nothing, keep polling because at lower limit 
-	subi r6, r6, 1
-	br toggle
-
-KEY3:
-	movi r6, 0
-	br toggle
+    
+    movia r8, 0xFF200000 # LEDs
+    movia r9, 0xFF200050 # Buttons (active low)
+    
+polling:
+    ldwio r2, 0(r9) # Read value from buttons into r2
+    
+    andi r3, r2, 0x1 # Extract the bit representing Push button 0
+    bne r3, r0, key0 # If button 0 is pressed, toggle LED 0
+    
+    andi r3, r2, 0x2 # Extract the bit representing Push button 1
+    bne r3, r0, key1 # If button 1 is pressed, toggle LED 1
+    
+    andi r3, r2, 0x4 # Extract the bit representing Push button 2
+    bne r3, r0, key2 # If button 2 is pressed, toggle LED 2
+    
+    andi r3, r2, 0x8 # Extract the bit representing Push button 3
+    bne r3, r0, key3 # If button 3 is pressed, toggle LED 3
+    
+    br polling # Loop back to start if no button is pressed
+    
+key0:
+    ldwio r4, 0(r8) # Read the status of the LEDs into r4
+    xori r4, r4, 0x1 # Toggle LED 0
+    stwio r4, 0(r8) # Update LEDs
+    br polling
+    
+key1:
+    ldwio r4, 0(r8) # Read the status of the LEDs into r4
+    xori r4, r4, 0x2 # Toggle LED 1
+    stwio r4, 0(r8) # Update LEDs
+    br polling
+    
+key2:
+    ldwio r4, 0(r8) # Read the status of the LEDs into r4
+    xori r4, r4, 0x4 # Toggle LED 2
+    stwio r4, 0(r8) # Update LEDs
+    br polling
+    
+key3:
+    ldwio r4, 0(r8) # Read the status of the LEDs into r4
+    xori r4, r4, 0x8 # Toggle LED 3
+    stwio r4, 0(r8) # Update LEDs
+    br polling
