f2a569c5d71ee63054690d90211724a7e2eebe78
Ashley N Leal <69736735+ashleyleal@users.noreply.github.com>
2024-02-03 21:58:36 -0500

    Update part2.s

--- Lab 4/part2.s
+++ Lab 4/part2.s
@@ -16,13 +16,13 @@ main:
     ldwio r2, 12(r9)         # Load the Edge Capture register value again
     bne r2, r0, toggle_state # If any bit is set, button was pressed
     beq r5, r0, update  # If counting state is active, update LEDs
-    br main             # Loop continuously
+    br main        
 
 toggle_state:
     movi r6, 1
     xor r5, r5, r6          
     stwio r2, 12(r9)         # Reset Edge Capture register bits to acknowledge button press
-    br main             # Return to the main loop
+    br main        
 
 update:
     call DELAY               
@@ -32,12 +32,11 @@ reset:
     movi r4, 0               # Reset counter to 0
 continue:
     stwio r4, 0(r8)          # Update LEDs with the counter value
-    br main		         	 # Continue the main loop
-
+    br main		         
 # Delay subroutine that waits 2.5s
 DELAY:
     movia r11, 500000       # Delay counter for CPUlator, adjust for actual hardware
 delay_loop:
     subi r11, r11, 1        # Decrement delay counter
     bne r11, r0, delay_loop # Loop until counter reaches 0
-    ret                     # Return from delay
+    ret                    
